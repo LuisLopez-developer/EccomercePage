15cfae547313145150d872b686a508461da73cfd
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-06-08 22:08:22 -0500

    se añadio un carrusel con cards, para mostrar los productos

---
+++ Models/ProductModel/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EccomercePage.Models.ProductModel
+{
+    public class ProductViewModel
+    {
+        public required string ImageSrc { get; set; }
+        public required string ImageAlt { get; set; }
+        public required string Name { get; set; }
+        public required decimal Price { get; set; }
+    }
+}
