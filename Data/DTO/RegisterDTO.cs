b0920b576c60f42c1c160dcd05373a9221a9b26a
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-25 13:10:33 -0500

    Inicialización de RegisterDTO y eliminación de required
    En RegisterDTO.cs, se eliminaron las propiedades required de
    UserName, Email, Password y ConfirmPassword, inicializándolas
    con string.Empty. En Register.razor, se inicializó registerDTO
    con una nueva instancia de RegisterDTO al declararla.

--- Data/DTO/RegisterDTO.cs
+++ Data/DTO/RegisterDTO.cs
@@ -2,9 +2,9 @@
 {
     public class RegisterDTO
     {
-        public required string UserName { get; set; }
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-        public required string ConfirmPassword { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
