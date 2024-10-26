d64efea3d9489547271ad8649e0aa2e601856959
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-26 10:20:31 -0500

    Añadir propiedad Id En el archivo `CartResumeDTO.cs`, se ha añadido una nueva propiedad `Id` de tipo `int` a la clase `CartItems`.

--- Data/DTO/CartDTO/CartResumeDTO.cs
+++ Data/DTO/CartDTO/CartResumeDTO.cs
@@ -9,6 +9,7 @@
 
     public class CartItems
     {
+        public int Id { get; set; }
         public int ProductId { get; set; }
         public string ProductName { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
